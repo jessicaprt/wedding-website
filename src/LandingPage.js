3f528195cb9060b9197ef07b845ed74d53114b07
Jessica Prieto <jessicaprieto31@gmail.com>
2018-10-13 14:59:26 -0400

    fixed registry text content

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -7,7 +7,7 @@ import { ContactInfo } from './ContactInfo.js';
 
 export class LandingPage extends Component {
     render() {
-        const registry="We're so lucky to be able to spend our wedding day with all of our friends and family. Your presence is truly all the present we need. Because we have all the basic household supplies covered, if you'd like to get us a gift to celebrate, we've created a registry fund that we'll use to enjoy our honeymoon.";
+        const registry="We're so lucky to be able to spend our wedding day with all of our friends and family. Your presence is truly all the present we need. Because we're living on a different province after the wedding and because we already have all the basic household supplies covered, if you'd like to get us a gift to celebrate, we've created a registry fund that we'll use to enjoy our honeymoon. You can either use the link below, or there will be dropboxes at the reception.";
         const accommodation="We are aware that a lot of our guests will be travelling from outside of Calgary. We have worked with Deerfoot Inn & Casino to give our guests discounts for room rates. To get the discount, you must confirm your attendance first by completing the RSVP form";
         return (
           <div className="App">
@@ -17,7 +17,9 @@ export class LandingPage extends Component {
             <PlainElement
               pid="registry"
               title="Registry" 
-              content={ registry }/>
+              content={ registry }
+              link="#"
+              linkText="available soon"/>
             <PlainElement 
               pid="accommodation"
               title="Guest Accommodations"
