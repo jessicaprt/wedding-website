5528f9a6672a08ad360d3fc686c615995a447894
George Antonious <george@antonious.ca>
2019-06-18 22:45:14 -0400

    Update thanks page

--- src/ThanksPage.js
+++ src/ThanksPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { PlainElement } from './PlainElement.js';
 var qs = require('qs');
 
 export class ThanksPage extends Component {
@@ -9,9 +10,25 @@ export class ThanksPage extends Component {
         let name = query.name
         let tableNumber = query.table
 
+        let content = <p>
+            Looks like you tried scanning the QR code 😛Thanks for making your way out to our wedding! It means a lot to us to be able to share this day with our closest family and friends. We hope you're having/had a wonderful time celebrating our special day.
+            <br/><br/>
+            Keep this QR code handy. We will post links to our wedding photos and video here when they are ready.
+            <br/><br/>
+            Again, thanks for coming from the Mr. and Mrs. Antonious family 💜
+        </p>
+
         return (
           <div className="App">
-              <h1>Thanks for coming to our wedding { name }!</h1>
+              <div className="Header">
+                <div className="headerTitle">
+                <PlainElement
+                    pid="thanks"
+                    title={`Thanks for coming ${name}!`}
+                    content={content}
+                    border="true"/>
+                </div>
+            </div>
           </div>
         );
       }
