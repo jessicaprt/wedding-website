b135103fbbdb5ae9c0597cd89d2e2c01b6d6b946
George Antonious <george@antonious.ca>
2019-06-18 21:37:25 -0400

    Add 404 page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { LandingPage } from './LandingPage';
 import { ThanksPage } from './ThanksPage';
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { NotFound } from './NotFound';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
@@ -11,21 +12,23 @@ class App extends Component {
       // </div>
       <BrowserRouter baseName={process.env.PUBLIC_URL}>
         <div>
-          <Route exact={true}
-            path='/'
-            render={() => (
-              <div className="App">
-                <LandingPage />
-              </div>
-          )}/>
-
-          <Route exact={true}
-            path='/thanks'
-            render={() => (
-              <div className="App">
-                <ThanksPage />
-              </div>
-          )}/>
+          <Switch>
+            <Route exact={true}
+              path='/'
+              render={() => (
+                <div className="App">
+                  <LandingPage />
+                </div>
+            )}/>
+            <Route exact={true}
+              path='/thanks'
+              render={() => (
+                <div className="App">
+                  <ThanksPage />
+                </div>
+            )}/>
+            <Route component={NotFound}/>
+          </Switch>
         </div>
       </BrowserRouter>
     );
---
+++ src/NotFound.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class NotFound extends Component {
+    render() {
+        return (
+          <div className="App">
+              <h1>404 :(</h1>
+          </div>
+        );
+      }
+}
